b66c92721d592ec50a74489b75995d4729e0848e
בר אופק <u9054555@bsmch.net>
2026-01-01 16:43:06 +0200

    done with tiod

--- app/src/main/java/com/example/task10_ya/MainActivity.java
+++ app/src/main/java/com/example/task10_ya/MainActivity.java
@@ -13,6 +13,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * @author Your Name
+ * @version 1.0
+ * @since 1/1/2026
+ * The main activity for the calculator application.
+ */
 public class MainActivity extends AppCompatActivity implements View.OnCreateContextMenuListener
 {
 
@@ -20,6 +31,14 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
     EditText etNum2;
     TextView tVoper;
 
+    /**
+     * Called when the activity is first created.
+     * <p>
+     * This is where you should do all of your normal static set up: create views, bind data to lists, etc.
+     * This method also provides you with a Bundle containing the activity's previously frozen state, if there was one.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -33,6 +52,16 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         tVoper.setOnCreateContextMenuListener(this);
 
     }
+
+    /**
+     * Called when the context menu for a view is about to be shown.
+     * <p>
+     * Use this method to add items to the context menu.
+     *
+     * @param menu The context menu that is being built
+     * @param v The view for which the context menu is being built
+     * @param menuInfo Extra information about the item for which the context menu should be shown. This information will vary depending on the class of v.
+     */
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
     {
@@ -42,6 +71,15 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         menu.add("*");
         menu.add("/");
     }
+
+    /**
+     * This hook is called whenever an item in a context menu is selected.
+     * <p>
+     * The default implementation simply returns false to allow normal context menu processing to proceed.
+     *
+     * @param item The context menu item that was selected.
+     * @return boolean Return false to allow normal context menu processing to proceed, true to consume it here.
+     */
     @Override
     public boolean onContextItemSelected(MenuItem item)
     {
@@ -87,6 +125,15 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
             tVoper.setText(String.valueOf(formatNiceResult(res)));
         return super.onContextItemSelected(item);
     }
+
+    /**
+     * Formats a double to a string, with special formatting for large and small numbers.
+     * <p>
+     * This method handles scientific notation for very large/small values and removes decimals for integers.
+     *
+     * @param num The number to format.
+     * @return String The formatted representation of the number.
+     */
     public String formatNiceResult(double num)
     {
         if (num == 0)
@@ -107,6 +154,13 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
             return String.format("%.3f", num);
         }
     }
+
+    /**
+     * Validates the input in the EditText fields.
+     * <p>
+     *
+     * @return boolean Return true if the input is valid, false otherwise.
+     */
     public boolean isValidInput()
     {
         String str1 = etNum1.getText().toString();
@@ -123,5 +177,3 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         return true;
     }
 }
-
-
